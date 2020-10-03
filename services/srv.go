166627f8d13a8fcb9d1a0ba9385c7b2ea4580d76
culturadevops <culturadevops@gmail.com>
2020-10-03 10:24:15 -0500

    pequeños cambios para ver variables del sistemas

--- services/srv.go
+++ services/srv.go
@@ -24,6 +24,7 @@ func (t *Srv) CreateMapDefault(fileName string) map[string]string {
 	return MapForReplace
 }
 func (t *Srv) GetTempleteName(origFolderName string, filesVersionName string) string {
+	//fmt.Println(viper.GetString("homedir") + "/" + origFolderName + "/" + filesVersionName + ".stub")
 	return viper.GetString("homedir") + "/" + origFolderName + "/" + filesVersionName + ".stub"
 }
 
